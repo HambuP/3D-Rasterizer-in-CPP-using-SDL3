9227f1053688bcaa1d7a15b36f5efc453a5deaa3
Santiago F <forerovar.santiago07@gmail.com>
2026-04-18 18:34:02 -0500

    CMakeLists.txt and SDL3 test window. Ya todo el setup para empezar el proyecto está perfectillo bello.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <SDL3/SDL.h>
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("Rasterizer", 800, 600, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) running = false;
+        }
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
